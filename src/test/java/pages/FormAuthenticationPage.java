b1be6df83e82c3abe66ab0bd59c8b7dbb171910d
ngan <44939177+nguyenngan918@users.noreply.github.com>
2020-10-10 10:58:09 +0700

    using page object model to defined login page (#1)

--- src/test/java/pages/FormAuthenticationPage.java
+++ src/test/java/pages/FormAuthenticationPage.java
@@ -6,22 +6,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FormAuthenticationPage {
     private static WebDriver driver;
-
     private final By usernameTXT = By.id("username");
     private final By passwordTXT = By.id("password");
     private final By loginBTN = By.xpath("//button[@type='submit']");
 
-    public static void open()
-    {
-        driver = new ChromeDriver();
+    public void open() {
         driver.get("https://the-internet.herokuapp.com/login");
     }
 
-    public static void fillFormAuthen()
-    {
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    public void fillForm(String username, String password) {
+        driver.findElement(usernameTXT).sendKeys(username);
+        driver.findElement(passwordTXT).sendKeys(password);
+        driver.findElement(loginBTN).click();
     }
 
 }
